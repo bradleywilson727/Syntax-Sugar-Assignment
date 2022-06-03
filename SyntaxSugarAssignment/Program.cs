f4d86e62edaf9ad18bed090ce1a15e998c0ba9cf
Bradley Wilson <bradleywilson727@gmail.com>
2022-06-03 10:27:18 -0500

    completed Syntax Sugar assignment

---
+++ SyntaxSugarAssignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SyntaxSugarAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            
+            var response = (answer < 9) ? $"{answer} is less then nine" : $"{answer} is greater than or equal to nine";
+
+            Console.WriteLine(response);
+        }
+    }
+}
